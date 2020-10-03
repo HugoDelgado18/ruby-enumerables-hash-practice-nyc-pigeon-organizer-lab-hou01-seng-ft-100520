9854b087334999353006df49d5887c72d48376d5
Hugo Delgado <hugo.delgado1@outlook.com>
2020-10-03 02:37:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,10 @@ def nyc_pigeon_organizer(data)
         if !new_array[name]
           new_array[name] = {}
         end
+        if !new_array[name][key]
+            new_array[name][key] = {}
         end
+
       end
      end
   new_array
